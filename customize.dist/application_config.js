837c185e7f8cef57d99f71cc29fb89a6f19cd5e3
Adrian Nöthlich <git@promasu.tech>
2018-11-25 12:37:51 +0100

    Disable userjoin notification

--- customize.dist/application_config.js
+++ customize.dist/application_config.js
@@ -3,12 +3,14 @@
  * The recommended method is to make a copy of this file (/customize.dist/application_config.js)
    in a 'customize' directory (/customize/application_config.js).
  * If you want to check all the configurable values, you can open the internal configuration file
-   but you should not change it directly (/common/application_config_internal.js)
+   but you should not change it directly (/common/application_config_internal.js
 */
 define(['/common/application_config_internal.js'], function (AppConfig) {
     config = AppConfig;
     config.displayCreationScreen = false;
     config.disableAnonymousStore = true;
+    config.disableUserlistNotifications = true;
+    config.hideLoadingScreenTips = true;
     config.availablePadTypes = ['drive', 'code', 'poll', 'kanban'];
     config.registeredOnlyTypes = ['drive', 'code', 'poll', 'kanban'];
     return AppConfig;
