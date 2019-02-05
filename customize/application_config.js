a61654a15cae4778207c0e01c9ba81b6785af346
Adrian Nöthlich <promasu@ox.io>
2019-02-05 17:30:04 +0000

    Make Instance available public

--- customize/application_config.js
+++ customize/application_config.js
@@ -12,6 +12,6 @@ define(['/common/application_config_internal.js'], function (AppConfig) {
     config.disableUserlistNotifications = true;
     config.hideLoadingScreenTips = true;
     config.availablePadTypes = ['drive', 'code', 'slide', 'contacts'];
-    config.registeredOnlyTypes = ['drive', 'code', 'slide', 'contacts'];
+    config.registeredOnlyTypes = ['drive'];
     return AppConfig;
 });
