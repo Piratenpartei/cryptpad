58a05e5a48bdb2eb0969e0b18ff087b36228b519
Adrian Nöthlich <git@promasu.tech>
2019-03-20 12:16:46 +0100

    Added custom limit for 2V HE

--- config.js
+++ config.js
@@ -218,6 +218,12 @@ module.exports = {
             limit: 500 * 1024 * 1024,
             plan: 'lavo',
             note: 'LaVo'
+        },
+
+        "https://cryptpad.piratenpartei.de/user/#/1/frecklesfairy/rwxqluVJDgUzHcOxy7a1hsJZ6dt+6dRLmfByJuoDM9Y=": {
+            limit: 500 * 1024 * 1024,
+            plan: 'lavo',
+            note: 'LaVo'
         }
 
     },
