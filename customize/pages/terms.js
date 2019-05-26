36e37120af94d3794d18d0f1aaa83607724e2c63
Adrian Nöthlich <git@promasu.tech>
2019-05-26 04:27:44 +0200

    Re-add normal customize page
    Signed-off-by: Adrian Nöthlich <git@promasu.tech>

--- customize/pages/terms.js
+++ customize/pages/terms.js
@@ -6,7 +6,14 @@ define([
     return function () {
         return h('div#cp-main', [
             Pages.infopageTopbar(),
-            Pages.setHTML(h('div.container.cp-container'), Msg.tos_content),
+            h('div.container.cp-container', [
+                h('center', h('h1', Msg.tos_title)),
+                h('p', Msg.tos_legal),
+                h('p', Msg.tos_availability),
+                h('p', Msg.tos_e2ee),
+                h('p', Msg.tos_logs),
+                h('p', Msg.tos_3rdparties),
+            ]),
             Pages.infopageFooter()
         ]);
     };
