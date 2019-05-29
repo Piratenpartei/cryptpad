58804fb3c40aeb8ea51afbbbf0f87e2b2680ae86
Adrian Nöthlich <git@promasu.tech>
2019-05-29 15:23:20 +0000

    Enable Slides Beta

--- customize/application_config.js
+++ customize/application_config.js
@@ -12,7 +12,7 @@ define(['/common/application_config_internal.js'], function (AppConfig) {
     config.disableUserlistNotifications = true;
     config.disableCrowdfundingMessages = true;
     config.hideLoadingScreenTips = true;
-    config.availablePadTypes = ['drive', 'code', 'pad', 'slide', 'file', 'todo', 'sheet', 'oodoc', 'contacts'];
+    config.availablePadTypes = ['drive', 'code', 'pad', 'slide', 'file', 'todo', 'oodoc', 'sheet', 'ooslide', 'contacts'];
     config.registeredOnlyTypes = ['contacts',];
     config.availableLanguages = ['de',];
     return AppConfig;
