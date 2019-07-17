9f52ef67c1c96c82c5550568eb3ea4d14e68af8d
Adrian Nöthlich <git@promasu.tech>
2019-07-17 23:55:18 +0200

    Update version string
    Signed-off-by: Adrian Nöthlich <git@promasu.tech>

--- customize/pages.js
+++ customize/pages.js
@@ -79,7 +79,7 @@ define([
                     ])
                 ])
             ]),
-            h('div.cp-version-footer', "CryptPad v2.24.0 (Yak)")
+            h('div.cp-version-footer', "CryptPad v2.25.0 (Zebra)")
         ]);
     };
 
